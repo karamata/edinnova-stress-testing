82c059f7513cafb2b0c987a88cdd55d4f8b25497
tran.phu <tran.phu@edinnova.com>
2019-11-14 09:47:18 +0700

    update test script

--- index.js
+++ index.js
@@ -39,68 +39,88 @@ const startBrowser = async email => {
     height: resolutionScreen[1] - 200,
   });
 
-  console.log('goto https://dev.edinnova.com/course-online/performance-test-demo/');
-  await page.goto('https://dev.edinnova.com/course-online/performance-test-demo/');
-
-  await sleep(5000);
-
-  console.log('click .edinnova-login');
-  await page.evaluate(() => {
-    document.querySelector('.edinnova-login').click();
-  });
-
-  await sleep(5000);
-
-  await page.evaluate((params) => {
-    document.querySelector('#username').value = params.email;
-    document.querySelector('#password').value = 'abc@123456';
-    document.querySelector('#kc-login').click();
-  }, {
-    email,
-  });
-
-  await sleep(10000);
-
-  await page.evaluate(() => {
-    document.querySelector('.course-nav-tab-curriculum > a:nth-child(1)').click();
-
-    document.querySelector('.lesson-title').click();
-  });
-
-  // await page.click('.lesson-title');
-
-  await sleep(10000);
-
-  pages = await browser.pages();
-
-  const classroomPage = pages[1];
-
-  classroomPage.setViewport({
-    width: resolutionScreen[0],
-    height: resolutionScreen[1] - 200,
-  });
-  
-  await classroomPage.evaluate(async () => {
-    const sleep = async (time = 3000) => {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, time);
-      })
-    }
+  try {
+    console.log('goto https://dev.edinnova.com/course-online/performance-test-demo/');
+    await Promise.all([
+      page.waitForNavigation({
+        timeout: 120000,
+        waitUntil: 'domcontentloaded',
+      }),
+      page.goto('https://dev.edinnova.com/course-online/performance-test-demo/')
+    ]);
 
     await sleep(5000);
 
-    document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
+    console.log('click .edinnova-login');
+    await page.evaluate(() => {
+      document.querySelector('.edinnova-login').click();
+    });
 
-    await sleep(5000);
+    await Promise.all([
+      page.waitForNavigation({
+        timeout: 120000,
+        waitUntil: 'domcontentloaded',
+      }),
+      sleep(5000)
+    ]);
 
-    document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
+    await page.evaluate((params) => {
+      document.querySelector('#username').value = params.email;
+      document.querySelector('#password').value = 'abc@123456';
+      document.querySelector('#kc-login').click();
+    }, {
+      email,
+    });
 
-    await sleep(5000);
+    await Promise.all([
+      page.waitForNavigation({
+        timeout: 120000,
+        waitUntil: 'domcontentloaded',
+      }),
+      await sleep(10000)
+    ]);
 
-    document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
-  });
+    await page.evaluate(() => {
+      document.querySelector('.course-nav-tab-curriculum > a:nth-child(1)').click();
+
+      document.querySelector('.lesson-title').click();
+    });
+
+    await sleep(10000);
+
+    pages = await browser.pages();
+
+    const classroomPage = pages[1];
+
+    classroomPage.setViewport({
+      width: resolutionScreen[0],
+      height: resolutionScreen[1] - 200,
+    });
+    
+    await classroomPage.evaluate(async () => {
+      const sleep = async (time = 3000) => {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve();
+          }, time);
+        })
+      }
+
+      await sleep(5000);
+
+      document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
+
+      await sleep(5000);
+
+      document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
+
+      await sleep(5000);
+
+      document.querySelector('.modal-dialog.wizard-dialog button.btn:nth-child(1)').click();
+    });
+  } catch (error) {
+    return [];
+  }
 
   return pages;
 };
@@ -108,7 +128,11 @@ const startBrowser = async email => {
 const startTesting = async () => {
   const pages = [];
   for (let idx = startIndex; idx <= lastIndex; idx++) {
-    const results = await startBrowser(`student.${idx}@edinnova.com`);
+    let results = await startBrowser(`student.${idx}@edinnova.com`);
+
+    while(results.length === 0) {
+      results = await startBrowser(`student.${idx}@edinnova.com`);
+    }
 
     pages.push(results);
   }
